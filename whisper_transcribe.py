a1c4a90a9b1d78d53a00b3d57c3991d9a99f54f7
David Pearl <davidtpearl@gmail.com>
2024-09-26 16:14:31 +1000

    Transcribe 2 more videos

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -1,10 +1,19 @@
 import os
 import whisper
+import time
+from datetime import timedelta
+
+# Record the start time
+start_time = time.time()
+
+# Print the results
+print(f"Start Time: {time.strftime('%H:%M:%S', time.localtime(start_time))}")
+
 
 MEDIA_FOLDER = 'media'
 TRANSCRIPTION_SAVE_LOCATION = 'output'
 AUDIO_FILE_TO_TRANSCRIBE = "sample-0.mp3"
-VIDEO_FILE_TO_TRANSCRIBE = "01-Module 1 - Funnel Rx - The Five Principles Of Funnel Building from Cathy Olson on Vimeo.mp4"
+VIDEO_FILE_TO_TRANSCRIBE = "02-Module 2 - Funnel Rx - Funnel Strategy Blueprints for 6 Common Business Models from Cathy Olson on Vimeo(1).mp4"
 FILE_TO_TRANSCRIBE = VIDEO_FILE_TO_TRANSCRIBE
 FILE_TO_TRANSCRIBE_WITH_FILE_PATH = f"{MEDIA_FOLDER}/{FILE_TO_TRANSCRIBE}"
 # Split the file name and extension
@@ -18,7 +27,21 @@ model = whisper.load_model("small")
 
 # Transcribe audio or video file
 result = model.transcribe(FILE_TO_TRANSCRIBE_WITH_FILE_PATH)
-print(result["text"])
+
+# Record the finish time
+finish_time = time.time()
+
+# Calculate the duration
+duration = finish_time - start_time
+
+# Convert duration to minutes and seconds
+duration_formatted = str(timedelta(seconds=duration))
+
+print("Transcription completed")
+
+print(f"Finish Time: {time.strftime('%H:%M:%S', time.localtime(finish_time))}")
+print(f"Duration: {duration_formatted}")
+
 # Write the transcription to a text file
 with open(f"{TRANSCRIPTION_SAVE_LOCATION}/{transcription_file_name}", "w") as text_file:
     text_file.write(result["text"])
